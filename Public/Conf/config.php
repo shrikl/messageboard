c1fea714dc7985c8a81c7b9118fb1c540c2b08d9
shrikl <553521882@qq.com>
2016-12-01 17:42:30 +0800

    Add files via upload

---
+++ Public/Conf/config.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'SHOW_PAGE_TRACE' => true,
+	'DB_DSN' => 'mysql://root:123456@localhost:3306/thinkphp',
+	'DB_PREFIX' => 'tp_', //添加表前缀 默认为"think_"
+	// 'LOAD_EXT_FILE' => 'functions', //加载项目函数库
+	// 'TMPL_TEMPLATE_SUFFIX' => '.htm', //伪静态后缀名
+	'TMPL_L_DELIM' => '<{',
+	'TMPL_R_DELIM' => '}>',
+	// 'TMPL_TEMPLATE_SUFFIX' => '.html', //更改模板文件后缀名
+	// 'DEFAULT_THEME'=>'my', //设置默认模板主题
+	// 'LAYOUT_ON' => true, //开启自动模板渲染
+	// 'URL_CASE_INSENSITIVE' => true,  //url不区分大小写
+	// 'URL_ROUTER_ON' => true,  //开启路由支持
+	// 'APP_GROUP_LIST' => 'Home,Admin', //项目分组设定
+);
+?>
